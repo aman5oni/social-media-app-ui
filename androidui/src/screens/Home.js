2a8fd0c1d0744ab549d8d8f5d93ef0ef11b3852d
Aman Soni <66624836+aman5oni@users.noreply.github.com>
2022-08-18 15:30:27 +0530

    "social media app"

---
+++ androidui/src/screens/Home.js
@@ -0,0 +1,237 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  TouchableOpacity
+} from "react-native";
+import { ScrollView, TextInput } from "react-native-gesture-handler";
+import Icon from "@expo/vector-icons/Entypo";
+import Posts from "./Posts";
+
+const Home = ({ navigation }) => {
+  const [state, setState] = useState({
+    popularSelected: true
+  });
+
+  const onTabPressed = () => {
+    setState({ popularSelected: !state.popularSelected });
+  };
+
+  return (
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      style={{
+        height: "100%",
+        backgroundColor: "#044244"
+      }}
+    >
+      <View
+        style={{
+          height: 260,
+          width: "100%",
+          paddingHorizontal: 35
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            width: "100%",
+            paddingTop: 40,
+            alignItems: "center"
+          }}
+        >
+          <View
+            style={{
+              width: "50%"
+            }}
+          >
+            <Image
+              source={require("../images/Untitled.png")}
+              style={{ width: 20, height: 20 }}
+            />
+          </View>
+          <View
+            style={{
+              width: "50%",
+              alignItems: "flex-end"
+            }}
+          >
+            <Icon
+              name="dots-two-vertical"
+              size={22}
+              color="#d2d2d2"
+              style={{
+                marginRight: -7,
+                marginTop: 7
+              }}
+            />
+          </View>
+        </View>
+
+        <Text
+          style={{
+            fontSize: 25,
+            color: "#FFF",
+            paddingVertical: 25
+          }}
+        >
+          Find Awesome Photos
+        </Text>
+
+        <View
+          style={{
+            flexDirection: "row",
+            borderColor: "#9ca1a2",
+            borderRadius: 20,
+            borderWidth: 0.2,
+            paddingVertical: 5,
+            alignItems: "center"
+          }}
+        >
+          <TextInput
+            placeholder="search inispiration ..."
+            style={{
+              paddingHorizontal: 20,
+              fontSize: 11,
+              width: "90%",
+              color: "#9ca1a2"
+            }}
+          />
+          <Icon name="magnifying-glass" size={15} color="#9ca1a2" />
+        </View>
+      </View>
+
+      <View
+        style={{
+          backgroundColor: "#FFF",
+          borderTopLeftRadius: 40,
+          borderTopRightRadius: 40,
+          height: 1000,
+          paddingHorizontal: 35
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            paddingTop: 20
+          }}
+        >
+          <TouchableOpacity
+            onPress={onTabPressed}
+            style={{
+              borderBottomColor: state.popularSelected ? "#044244" : "#FFF",
+              borderBottomWidth: 4,
+              paddingVertical: 6
+            }}
+          >
+            <Text
+              style={{
+                color: state.popularSelected ? "#044244" : "#9ca1a2"
+              }}
+            >
+              MOST POPULAR
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={onTabPressed}
+            style={{
+              borderBottomColor: state.popularSelected ? "#FFF" : "#044244",
+              borderBottomWidth: 4,
+              paddingVertical: 6,
+              marginLeft: 30
+            }}
+          >
+            <Text
+              style={{
+                color: state.popularSelected ? "#9ca1a2" : "#044244"
+              }}
+            >
+              RECENT
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row"
+          }}
+        >
+          <Posts
+            onPress={() => navigation.navigate("Detail")}
+            name="Max Bator"
+            profile={require("../images/p1.jpg")}
+            photo={require("../images/5.jpg")}
+          />
+
+          <View
+            style={{
+              height: 160,
+              backgroundColor: "#3c636c",
+              width: 20,
+              marginLeft: 20,
+              marginTop: 120,
+              borderTopLeftRadius: 20,
+              borderBottomLeftRadius: 20
+            }}
+          ></View>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row"
+          }}
+        >
+          <View
+            style={{
+              height: 160,
+              backgroundColor: "#3c636c",
+              width: 20,
+              marginLeft: -40,
+              marginRight: 20,
+              marginTop: 120,
+              borderTopRightRadius: 20,
+              borderBottomRightRadius: 20
+            }}
+          ></View>
+
+          <Posts
+            onPress={() => navigation.navigate("Detail")}
+            name="Erka Berka"
+            profile={require("../images/p2.jpg")}
+            photo={require("../images/6.jpg")}
+          />
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row"
+          }}
+        >
+          <Posts
+            onPress={() => navigation.navigate("Detail")}
+            name="Max Bator"
+            profile={require("../images/p1.jpg")}
+            photo={require("../images/3.jpg")}
+          />
+
+          <View
+            style={{
+              height: 160,
+              backgroundColor: "#3c636c",
+              width: 20,
+              marginLeft: 20,
+              marginTop: 120,
+              borderTopLeftRadius: 20,
+              borderBottomLeftRadius: 20
+            }}
+          ></View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default Home;
